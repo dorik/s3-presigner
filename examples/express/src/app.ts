e9f769e69e7876f0977cc3340048e1b65dcf0a01
Anwar Hussain <anwar828@gmail.com>
2023-06-07 20:10:16 +0600

    fix: remove static bucket name

--- examples/express/src/app.ts
+++ examples/express/src/app.ts
@@ -3,12 +3,14 @@ import { createS3PresignedUrl } from "@s3-presigner/server";
 
 const app = express();
 
+const bucketName = process.env.BUCKET_NAME as string;
+
 app.post(
   "/presigned-url",
   express.text(),
   async (req: Request, res: Response) => {
     const { getUploadUrl } = createS3PresignedUrl({
-      bucket: "cms-api",
+      bucket: bucketName,
     });
     const data = await getUploadUrl(req.body);
 
@@ -20,6 +22,6 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello, Express!");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
 });
--- examples/nextjs/src/pages/api/upload.ts
+++ examples/nextjs/src/pages/api/upload.ts
@@ -1,11 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { createS3PresignedUrl } from "@s3-presigner/server";
 
+const bucketName = process.env.BUCKET_NAME as string;
+
 export default async function uploadHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { getUploadUrl } = createS3PresignedUrl({ bucket: "cms-api" });
+  const { getUploadUrl } = createS3PresignedUrl({
+    bucket: bucketName,
+  });
   const data = await getUploadUrl(req.body);
 
   return res.json(data);
